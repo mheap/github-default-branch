0419bb245afb29de34646ad04dcfe77bd4f32df3
Michael Heap <m@michaelheap.com>
2020-06-15 09:36:59 +0100

    Add --user support

--- src/get-repos.js
+++ src/get-repos.js
@@ -3,9 +3,22 @@ module.exports = async function (args, octokit) {
     return [args.repo];
   }
 
-  const { data: repos } = await octokit.repos.listForOrg({
-    org: args.org,
-  });
+  let repos;
+  if (args.org) {
+    ({ data: repos } = await octokit.repos.listForOrg({
+      org: args.org,
+    }));
+  }
+
+  if (args.user) {
+    ({ data: repos } = await octokit.repos.listForAuthenticatedUser());
+
+    // Filter down to repos owned by the provided user
+    // This is different to using affiliation: owner as it allows
+    // the consumer to update repos that they have admin access to
+    // but do not own
+    repos = repos.filter((repo) => repo.owner.login == args.user);
+  }
 
   return repos.map((repo) => repo.full_name);
 };
