5aa9a0db8f2f457d2308d02aa24c1ca3d598742d
Michael Heap <m@michaelheap.com>
2020-12-20 17:55:43 +0000

    Add Throttling support

--- src/octokit.js
+++ src/octokit.js
@@ -10,8 +10,31 @@ module.exports = (argv) => {
     OctokitClass = Octokit.plugin(enterpriseCompatibility);
   }
 
+  // Add Throttling support
+  const { throttling } = require("@octokit/plugin-throttling");
+  OctokitClass = Octokit.plugin(throttling);
+
   let octokitArgs = {
     auth: argv.pat || process.env.GITHUB_TOKEN,
+    throttle: {
+      onRateLimit: (retryAfter, options) => {
+        console.error(
+          `Request quota exhausted for request ${options.method} ${options.url}`
+        );
+
+        // Retry 3 times
+        if (options.request.retryCount < 3) {
+          console.error(`Retrying after ${retryAfter} seconds!`);
+          return true;
+        }
+      },
+      onAbuseLimit: (retryAfter, options, octokit) => {
+        // does not retry, only logs a warning
+        console.error(
+          `Abuse detected for request ${options.method} ${options.url}`
+        );
+      },
+    },
   };
 
   if (argv.baseUrl) {
