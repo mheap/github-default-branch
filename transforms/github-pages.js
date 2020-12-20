0627cd24e4deb5fbb2d3a5247ba3db5dc3127f03
Michael Heap <m@michaelheap.com>
2020-12-20 18:21:39 +0000

    Update GitHub Pages config

---
+++ transforms/github-pages.js
@@ -0,0 +1,43 @@
+module.exports = async function ({
+  owner,
+  repo,
+  old,
+  target,
+  octokit,
+  log,
+  dryRun,
+}) {
+  log = log.extend("transforms:github-pages");
+
+  log("Updating GitHub Pages config");
+
+  try {
+    const {
+      data: { source },
+    } = await octokit.repos.getPages({
+      owner,
+      repo,
+    });
+
+    if (source.branch != old) {
+      log(
+        `Skipping GitHub pages as [${source.branch}] does not match [${old}]`
+      );
+      return;
+    }
+
+    source.branch = target;
+
+    await octokit.repos.updateInformationAboutPagesSite({
+      owner,
+      repo,
+      source,
+    });
+
+    log(
+      `Updated GitHub pages from [${old}] to [${target}] with path [${source.path}]`
+    );
+  } catch (e) {
+    log(`No GitHub Pages found for [${owner}/${repo}]`);
+  }
+};
---
+++ transforms/github-pages.test.js
@@ -0,0 +1,115 @@
+const nock = require("nock");
+nock.disableNetConnect();
+
+const log = require("../src/mock-debug");
+const githubPages = require("./github-pages");
+
+const createBranch = require("./github-pages");
+const octokit = require("../src/octokit")();
+
+afterEach(() => {
+  if (!nock.isDone()) {
+    throw new Error("Not all nock interceptors were used");
+  }
+  log.cleanAll();
+  nock.cleanAll();
+});
+
+describe("#github-pages", () => {
+  it("continues with a log if pages is not enabled", async () => {
+    nock("https://api.github.com/").get("/repos/demo/repo/pages").reply(404);
+
+    await githubPages({
+      owner: "demo",
+      repo: "repo",
+      old: "master",
+      target: "main",
+      octokit,
+      log,
+    });
+
+    expect(log.logger).toBeCalledWith("No GitHub Pages found for [demo/repo]");
+  });
+
+  it("logs if the configured branch does not match the old branch", async () => {
+    nock("https://api.github.com/")
+      .get("/repos/demo/repo/pages")
+      .reply(200, {
+        source: {
+          branch: "custom",
+          path: "/",
+        },
+      });
+
+    await githubPages({
+      owner: "demo",
+      repo: "repo",
+      old: "master",
+      target: "main",
+      octokit,
+      log,
+    });
+
+    expect(log.logger).toBeCalledWith(
+      "Skipping GitHub pages as [custom] does not match [master]"
+    );
+  });
+
+  it("configures actions with the new branch", async () => {
+    nock("https://api.github.com/")
+      .get("/repos/demo/repo/pages")
+      .reply(200, {
+        source: {
+          branch: "master",
+          path: "/",
+        },
+      });
+
+    nock("https://api.github.com/")
+      .put("/repos/demo/repo/pages", { source: { branch: "main", path: "/" } })
+      .reply(200);
+
+    await githubPages({
+      owner: "demo",
+      repo: "repo",
+      old: "master",
+      target: "main",
+      octokit,
+      log,
+    });
+
+    expect(log.logger).toBeCalledWith(
+      "Updated GitHub pages from [master] to [main] with path [/]"
+    );
+  });
+
+  it("configures actions with the new branch (custom path)", async () => {
+    nock("https://api.github.com/")
+      .get("/repos/demo/repo/pages")
+      .reply(200, {
+        source: {
+          branch: "master",
+          path: "/docs",
+        },
+      });
+
+    nock("https://api.github.com/")
+      .put("/repos/demo/repo/pages", {
+        source: { branch: "main", path: "/docs" },
+      })
+      .reply(200);
+
+    await githubPages({
+      owner: "demo",
+      repo: "repo",
+      old: "master",
+      target: "main",
+      octokit,
+      log,
+    });
+
+    expect(log.logger).toBeCalledWith(
+      "Updated GitHub pages from [master] to [main] with path [/docs]"
+    );
+  });
+});
